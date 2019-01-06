0f8bbbd024bd29ad815c92d671b058817ae4ecd7
HOT5 <daniil.ikol@gmail.com>
2019-01-06 21:03:33 +0200

    Added State package

---
+++ core/src/com/mygdx/game/States/GameStateManager.java
@@ -0,0 +1,37 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+public class GameStateManager {
+
+    private Stack<State> states;
+
+    public GameStateManager() {
+        states = new Stack<State>();
+    }
+
+    public void push(State state) {
+        states.push(state);
+    }
+
+    public void pop(State state) {
+        states.pop().dispose();
+    }
+
+    public void set(State state) {
+        states.pop().dispose();
+        states.push(state);
+    }
+
+    public void update(float deltaTime) {
+        states.peek().update(deltaTime);
+    }
+
+    public void render(SpriteBatch batch)
+    {
+        states.peek().render(batch);
+    }
+
+}
---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,30 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float deltatime) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch batch) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
